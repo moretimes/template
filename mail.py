92f056ad4f7d9b9e4d9bf468019b1e82423a467a
moretimes <moretimes@126.com>
2015-12-24 11:33:06 +0800

    mail.py

--- mail.py
+++ mail.py
@@ -1,31 +1,51 @@
-import smtplib  
-from email.MIMEText import MIMEText
-from email.Header import Header
-mailto_list=["moretimes@126.com", "moretimes@126.com"]
-mail_host="smtp.163.com"
-mail_user="sysuacmm"
-mail_pass="acmmcb"
-mail_postfix="163.com"
-  
+# -*- coding: UTF-8 -*-
+'''
+发送txt文本邮件
+小五义：http://www.cnblogs.com/xiaowuyi
+'''
+import smtplib
+from email.mime.text import MIMEText
+from time import sleep
+mailto_list=[]
+mail_host="smtp.126.com"  #设置服务器
+mail_user="moretimes"    #用户名
+mail_pass="wangtianyu&1995"   #口令
+mail_postfix="126.com"  #发件箱的后缀
+
 def send_mail(to_list,sub,content):
-    me="hello"+"<"+mail_user+"@"+mail_postfix+">"
-    msg = MIMEText(content,_subtype='html',_charset='gb2312')
+    me="SYSUACMM"+"<"+mail_user+"@"+mail_postfix+">"
+    msg = MIMEText(content,_subtype='plain',_charset='utf-8')
     msg['Subject'] = sub
     msg['From'] = me
     msg['To'] = ";".join(to_list)
     try:
-        s = smtplib.SMTP()
-        s.connect(mail_host)
-        s.login(mail_user,mail_pass)
-        s.sendmail(me, to_list, msg.as_string())
-        s.close()
+        server = smtplib.SMTP()
+        server.connect(mail_host)
+        server.login(mail_user,mail_pass)
+        server.sendmail(me, to_list, msg.as_string())
+        server.close()
         return True
     except Exception, e:
         print str(e)
         return False
 if __name__ == '__main__':
-    if send_mail(mailto_list,"hello","Test Content"):
-        print "Success"
-    else:
-        print "Fail"
+    f = open('content', 'r')
+    s = f.read();
+    add = open('address', 'r')
+    cnt = 500
+    while True:
+        cnt = cnt + 1
+        term = add.readline()
+        if not term:
+            break
+        print cnt
+        if send_mail([term],"新手赛网络预选赛", s):
+            print term + "发送成功"
+        else:
+            print term + "发送失败"
+        sleep(5)
+
+   # for a in mailto_list:
+   #    print a
+    #print mailto_list;
 
